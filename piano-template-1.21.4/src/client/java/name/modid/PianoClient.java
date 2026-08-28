622ae06f340d9b875649d954c64903aab23ecf3f
kipd0 <laith28dev@gmail.com>
2026-08-28 22:46:01 +0300

    Update PianoClient.java

--- piano-template-1.21.4/src/client/java/name/modid/PianoClient.java
+++ piano-template-1.21.4/src/client/java/name/modid/PianoClient.java
@@ -1,3 +1,52 @@
+package name.modid;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
+
+public class PianoClient implements ClientModInitializer {
+
+    public static KeyMapping OPEN_NOTE_BLOCKS;
+    public static KeyMapping OPEN_SONG_EDITOR;
+    public static KeyMapping PLAY_SONG;
+
+    public static final PianoConfig CONFIG = new PianoConfig();
+    public static final SongPlayer PLAYER = new SongPlayer();
+
+    @Override
+    public void onInitializeClient() {
+
+        OPEN_NOTE_BLOCKS = KeyBindingHelper.registerKeyBinding(
+                new KeyMapping(
+                        "key.piano.note_blocks",
+                        InputConstants.Type.KEYSYM,
+                        GLFW.GLFW_KEY_N,
+                        "category.piano"
+                )
+        );
+
+        OPEN_SONG_EDITOR = KeyBindingHelper.registerKeyBinding(
+                new KeyMapping(
+                        "key.piano.song_editor",
+                        InputConstants.Type.KEYSYM,
+                        GLFW.GLFW_KEY_M,
+                        "category.piano"
+                )
+        );
+
+        PLAY_SONG = KeyBindingHelper.registerKeyBinding(
+                new KeyMapping(
+                        "key.piano.play_song",
+                        InputConstants.Type.KEYSYM,
+                        GLFW.GLFW_KEY_P,
+                        "category.piano"
+                )
+        );
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
 
             while (OPEN_NOTE_BLOCKS.consumeClick()) {
@@ -13,7 +62,6 @@
             }
 
             while (PLAY_SONG.consumeClick()) {
-
                 if (PLAYER.isPlaying()) {
                     PLAYER.stop();
                 } else {
@@ -25,3 +73,5 @@
         });
 
         NoteBlockRenderer.register();
+    }
+}
