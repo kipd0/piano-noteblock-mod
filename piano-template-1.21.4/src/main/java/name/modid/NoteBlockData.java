116d24517f62850b76b1d764eab0c25893db23fe
kipd0 <laith28dev@gmail.com>
2026-08-28 21:24:53 +0300

    Create NoteBlockData.java

---
+++ piano-template-1.21.4/src/main/java/name/modid/NoteBlockData.java
@@ -0,0 +1,17 @@
+package name.modid;
+
+public class NoteBlockData {
+
+    public int x;
+    public int y;
+    public int z;
+
+    public NoteBlockData() {
+    }
+
+    public NoteBlockData(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
