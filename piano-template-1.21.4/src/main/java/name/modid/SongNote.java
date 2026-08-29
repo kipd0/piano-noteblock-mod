66cca364744d7b70b8d9ddbdb765104099886c9d
kipd0 <laith28dev@gmail.com>
2026-08-29 10:45:19 +0300

    Update SongNote.java

--- piano-template-1.21.4/src/main/java/name/modid/SongNote.java
+++ piano-template-1.21.4/src/main/java/name/modid/SongNote.java
@@ -3,13 +3,11 @@ package name.modid;
 public class SongNote {
 
     public int noteBlock;
-    public long durationMs;
 
     public SongNote() {
     }
 
-    public SongNote(int noteBlock, long durationMs) {
+    public SongNote(int noteBlock) {
         this.noteBlock = noteBlock;
-        this.durationMs = durationMs;
     }
 }
